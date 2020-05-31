1644aa53f245454ef0ac01c6d755eead088dad2c
Patricio Perez <p.perez.bustos@accenture.com>
2020-05-31 16:37:36 -0400

    Added Swagger documentation

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 
 const Hapi = require('@hapi/hapi');
 
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+
+const Pack = require('./package');
+
 const init = async () => {
     
     const server = Hapi.server({
@@ -11,6 +17,32 @@ const init = async () => {
     
     //Get Routes.
     server.route(require('./routes'));
+
+    //Set Swagger
+    const swaggerOptions = {
+        info: {
+                title: 'Simple CRUD API Documentation',
+                version: Pack.version,
+            },
+        };
+
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    //Swagger documentation redirect.
+    server.route({
+        method: 'GET',
+        path: "/",
+        handler: function (request, h) {
+            return h.redirect(`${request.url.origin}/documentation`)
+        }
+    });
 
     //404 error
     server.route({
--- routes/bodega.js
+++ routes/bodega.js
@@ -12,6 +12,21 @@ const Joi = require('@hapi/joi');
 const mongoSrv = process.env.MONGOSRV ?? "localhost"
 const connection = `mongodb://${mongoSrv}:27017/bodega`;
 
+const guidExample = "f4b23110-a319-11ea-94a8-835ccf0337c7";
+const skuExample = "ABCDEF1234";
+const productExample = "Producto X";
+const cantidadExample = 10;
+const dateExample = 1590914246305;
+
+const articuloSchema = Joi.object()
+    .keys({
+        _id: Joi.string().guid().example(guidExample),
+        sku: Joi.string().min(10).max(10).required().example(skuExample),
+        descripcion: Joi.string().min(10).max(100).required().example(productExample),
+        cantidad: Joi.number().required().example(cantidadExample),
+        fecha_ingreso: Joi.date().example(dateExample)
+    }).label('Articulo');
+
 //Simple CRUD in REST:
 //GET -> READ
 //POST -> CREATE
@@ -21,6 +36,24 @@ module.exports = [
     {
         method: 'GET',
         path: '/articulo',
+        options: {
+            description: 'Show all articles registered',
+            notes: 'Returns all articles registered in the storage.',
+            tags: ['api'], // ADD THIS TAG
+            plugins: {
+                'hapi-swagger':{
+                    responses: {
+                        '200': {
+                            description: "Returns array of articles",
+                            schema: Joi.array().items(articuloSchema).label('Articulos')
+                        },
+                        '400': {
+                            description: "Problems connecting to DB",
+                        }
+                    }
+                }
+            }
+        },
         handler: async (request, h) => {
             const db = mongojs(connection);
             
@@ -55,16 +88,32 @@ module.exports = [
     {
         method: 'POST',
         path: '/articulo',
-        config: {
+        options: {
+            description: 'Add new articule',
+            notes: 'Add an article to storage.',
+            tags: ['api'], // ADD THIS TAG
+            plugins: {
+                'hapi-swagger':{
+                    responses: {
+                        '200': {
+                            description: "Return registered article",
+                            schema: articuloSchema
+                        },
+                        '400': {
+                            description: "Problems connecting to DB",
+                        }
+                    }
+                }
+            },
             validate: {
                 payload: Joi.object({
-                    //sku is defined as ABC-CDE-1234
-                    sku: Joi.string().min(10).max(10).required(),
+                    //sku is defined as ABCCDE1234. This will be represented as ABC-CDE-1234.
+                    sku: Joi.string().min(10).max(10).required().example(skuExample),
                     //descripcion will be defined between 10 and 255 characters
-                    descripcion: Joi.string().min(10).max(100).required(),
+                    descripcion: Joi.string().min(10).max(100).required().example(productExample),
                     //cantidad is an integer
-                    cantidad: Joi.number().required()
-                }),
+                    cantidad: Joi.number().required().example(cantidadExample)
+                }).label('AddArticulo'),
                 failAction: async (request, h, err) => {
                     console.error(err);
                     throw err;
@@ -114,18 +163,40 @@ module.exports = [
     {
         method: 'PATCH',
         path: '/articulo/{id}',
-        config: {
+        options: {
+            description: 'Update an existing articule',
+            notes: 'Update values for existing article',
+            tags: ['api'], // ADD THIS TAG
+            plugins: {
+                'hapi-swagger':{
+                    responses: {
+                        '200': {
+                            description: "Return patch status.",
+                            schema: Joi.object().keys({
+                                n: Joi.number().example(1),
+                                nModified: Joi.number().example(0),
+                                ok: Joi.number().example(1)
+                            }).label('PatchStatus')
+                        },
+                        '400': {
+                            description: "Problems connecting to DB",
+                        }
+                    }
+                }
+            },
             validate: {
                 payload: Joi.object({
                     //sku is defined as ABC-CDE-1234
-                    sku: Joi.string().min(10).max(10),
+                    sku: Joi.string().min(10).max(10).example(skuExample),
                     //descripcion will be defined between 10 and 255 characters
-                    descripcion: Joi.string().min(10).max(100),
+                    descripcion: Joi.string().min(10).max(100).example(productExample),
                     //cantidad is an integer
-                    cantidad: Joi.number()
-                }),
+                    cantidad: Joi.number().example(cantidadExample),
+                    // fecha_ingreso is a date.
+                    fecha_ingreso: Joi.date().example(dateExample)
+                }).label('UpdateArticulo'),
                 params: Joi.object({
-                    id: Joi.string().guid()
+                    id: Joi.string().guid().example(guidExample).description('GUID code')
                 }),
                 failAction: async (request, h, err) => {
                     console.error(err);
@@ -174,9 +245,29 @@ module.exports = [
         method: 'DELETE',
         path: '/articulo/{id}',
         options: {
+            description: 'Delete an existing articule',
+            notes: 'Delete existing article',
+            tags: ['api'], // ADD THIS TAG
+            plugins: {
+                'hapi-swagger':{
+                    responses: {
+                        '200': {
+                            description: "Return delete status.",
+                            schema: Joi.object().keys({
+                                n: Joi.number().example(1),
+                                ok: Joi.number().example(1),
+                                deletedCount: Joi.number().example(1)
+                            }).label('DeleteStatus')
+                        },
+                        '400': {
+                            description: "Problems connecting to DB",
+                        }
+                    }
+                }
+            },
             validate: {
                 params: Joi.object({
-                    id: Joi.string().guid()
+                    id: Joi.string().guid().example(guidExample).description('GUID code')
                 })
             }
         },
